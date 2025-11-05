4bcfa9b3bcdceb3134ae87259da836f405402710
Liam212 <liamstout212@gmail.com>
2025-11-05 19:03:37 +0000

    add posthog

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { PostHogProvider } from 'posthog-js/react'
 
 import { routeTree } from './routeTree.gen'
 
@@ -29,10 +30,20 @@ if (!rootElement.innerHTML) {
   const root = createRoot(rootElement)
   root.render(
     <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools buttonPosition="bottom-right" />
-      </QueryClientProvider>
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={{
+          api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+          defaults: '2025-05-24',
+          capture_exceptions: true,
+          debug: import.meta.env.MODE === 'development',
+        }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools buttonPosition="bottom-right" />
+        </QueryClientProvider>
+      </PostHogProvider>
     </StrictMode>,
   )
 }
